0b7abd91f7fee494952c012818200785ae24d518
Narayan Shirodkar <nshirodkar@falabella.cl>
2024-03-20 00:27:58 +0530

    Bubble sort

---
+++ Sorting/bubbleSort.js
@@ -0,0 +1,32 @@
+// A sorting algorithm where the largest values bubble up to the top.
+
+// function bubbleSort(arr) {
+//   for (let i = arr.length; i > 0; i--) {
+//     for (let j = 0; j < i - 1; j++) {
+//       if (arr[j] > arr[j + 1]) {
+//         [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+//       }
+//     }
+//   }
+//   return arr;
+// }
+
+//Omptimized code
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--) {
+    let noSwap = true;
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+        noSwap = false;
+      }
+    }
+    if (noSwap) break;
+  }
+  return arr;
+}
+
+console.log(bubbleSort([5, 3, 7, 1, 6, 2]));
+
+// Time Complexity => O(n2)
+// Space Complexity => O(1)
